14ceb7e276d5814f444fd826a26fa6656f79192f
blueskyer1998 <117384225+blueskyer1998@users.noreply.github.com>
2025-03-08 17:16:10 +0800

    Time: 7 ms (76.86%) | Memory: 44.7 MB (69.05%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        if (word1.equals(word2)) {
+            return 0;
+        } 
+
+        return valid(word1, word2);
+    }
+
+    
+    private static int valid(String word1, String word2) {
+        int maxLength = 0;
+
+        List<String> list = new ArrayList<>();
+
+        if(word1.length()>word2.length()){
+            String temp = word1;
+            word1 = word2;
+            word2 = temp;
+        }
+
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+
+        for (int i = 0; i <= word1.length(); i++) {
+            for (int j = 0; j <= word2.length(); j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                   dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        maxLength = dp[word1.length()][word2.length()];
+        return word1.length() - maxLength + word2.length() - maxLength;
+    }
+}
